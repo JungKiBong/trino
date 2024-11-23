66a7d3ce5125617a09d00b2f54735e2121fe786d
JungKiBong <jkbrown8408@gmail.com>
2024-11-23 14:04:33 +0900

    init

--- docmost/apps/server/src/integrations/export/export.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExportService } from './export.service';
-import { ImportController } from './export.controller';
-
-@Module({
-  providers: [ExportService],
-  controllers: [ImportController],
-})
-export class ExportModule {}
